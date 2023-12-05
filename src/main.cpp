cd74cbecb64763ca8fcd313d8e01eca26a9a7c87
Ryan Smith <ryan.smith@ucdconnect.ie>
2023-12-05 17:30:09 +0000

    test new print func

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,10 @@ auto main(int argc, char** argv) -> int {
     Player player1{"White", 1500.0, 'w'};
     Player player2{"Black", 1600.0, 'b'};
 
-    Board board;
-    board.print_position();
+    Board board{};
+    board.print();
 
     board.move("d4");
 
-    board.print_position();
+    board.print();
 }
