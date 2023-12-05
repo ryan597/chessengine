10cd0d5322cfab680760c9ce577ca20738d5d0ed
Ryan Smith <ryan.smith@ucdconnect.ie>
2023-12-05 17:52:12 +0000

    need to investigate bug with get_file..

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,12 @@ auto main(int argc, char** argv) -> int {
     Player player2{"Black", 1600.0, 'b'};
 
     Board board{};
+
+    Pawn* apawn = new Pawn('w', Square{'a', '2'});
+    std::cout << apawn->get_file();
+    delete apawn;
+
+    board.print_fen();
     board.print();
 
     board.move("d4");
