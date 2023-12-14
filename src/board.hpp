9c404844f040bbf0a6a6da07ebd4462567aed14d
Ryan Smith <ryan.smith@ucdconnect.ie>
2023-12-14 09:39:50 +0000

    format board

--- src/board.hpp
+++ src/board.hpp
@@ -1,10 +1,10 @@
 #ifndef H_BOARD
 #define H_BOARD
 
-#include <iostream>
 #include <algorithm>
-#include <random>
+#include <iostream>
 #include <memory>
+#include <random>
 
 #include "piece.hpp"
 
@@ -12,25 +12,24 @@ class Piece;
 
 class Board {
 private:
-    std::string m_current_fen;
-    // Vector of pieces on board
-    std::vector<std::unique_ptr<Piece>> m_pieces;
-    char m_turn;
+  std::string m_current_fen;
+  // Vector of pieces on board
+  std::vector<std::unique_ptr<Piece>> m_pieces;
+  char m_turn;
 
 public:
-    Board();
-    Board(int variant);
-    auto get_fen() -> std::string;
-    auto set_fen(const std::string& fen_string) -> bool;
-    auto static check_fen_is_valid(const std::string& fen) -> bool;
-    auto print() -> void;
-    auto print_fen() -> void;
-    auto static print_position(std::string& fen) -> void;
-    auto is_legal_move(const std::string& notation) -> bool;
-    auto move(const std::string& notation) -> bool;
-    auto parse_move(const std::string& move) -> void;
-    auto get_pieces() -> std::vector<std::unique_ptr<Piece>>&;
+  Board();
+  Board(int variant);
+  auto get_fen() -> std::string;
+  auto set_fen(const std::string &fen_string) -> bool;
+  auto static check_fen_is_valid(const std::string &fen) -> bool;
+  auto print() -> void;
+  auto print_fen() -> void;
+  auto static print_position(std::string &fen) -> void;
+  auto is_legal_move(const std::string &notation) -> bool;
+  auto move(const std::string &notation) -> bool;
+  auto parse_move(const std::string &move) -> void;
+  auto get_pieces() -> std::vector<std::unique_ptr<Piece>> &;
 };
-
 
 #endif
