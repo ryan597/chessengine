3784f8742ead85b75308608137344440a3b1d642
Ryan Smith <ryan.smith@ucdconnect.ie>
2023-12-14 09:40:07 +0000

    format player

--- src/player.hpp
+++ src/player.hpp
@@ -5,18 +5,19 @@
 
 class Player {
 private:
-    std::string m_name;
-    float m_rating;
-    char m_colour;
+  std::string m_name;
+  float m_rating;
+  char m_colour;
+
 public:
-    Player(std::string name, float rating, char colour) :
-        m_name(std::move(name)), m_rating(rating), m_colour(colour) {}
-    auto get_name() -> std::string;
-    auto set_name(std::string name) -> void;
-    auto get_rating() -> float;
-    auto set_rating(float rating) -> void;
-    auto get_colour() -> char;
-    auto set_colour(char colour) -> void;
+  Player(std::string name, float rating, char colour)
+      : m_name(std::move(name)), m_rating(rating), m_colour(colour) {}
+  auto get_name() -> std::string;
+  auto set_name(std::string name) -> void;
+  auto get_rating() -> float;
+  auto set_rating(float rating) -> void;
+  auto get_colour() -> char;
+  auto set_colour(char colour) -> void;
 };
 
 #endif
